e22b83a4b54d9bfc7ff3a273333794130df06949
Chaitanya Lohani <chaitanyalohani175@gmail.com>
2024-12-06 14:55:56 +0530

    Update email configuration placeholders in settings.py

--- FullStack_website/settings.py
+++ FullStack_website/settings.py
@@ -138,5 +138,5 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "chaitanyalohani175@gmail.com"  # Your email
-EMAIL_HOST_PASSWORD = "safv mcjr mzog vxum"  # Gmail app password
+EMAIL_HOST_USER = "your email"
+EMAIL_HOST_PASSWORD = "your password"  # Gmail app password
