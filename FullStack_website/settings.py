60986ba8474e51dfc6aeac1ecdc113d6044f6eb1
Chaitanya Lohani <chaitanyalohani175@gmail.com>
2025-01-18 16:41:24 +0530

    Remove hardcoded secret key from settings.py for enhanced security

--- FullStack_website/settings.py
+++ FullStack_website/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-ilm0wrq@#f2%*)8(69#20+$u!gs##4u(85av)(bak5g@pknsecure-v+-99'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
